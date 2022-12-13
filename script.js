d9fb93d92bac85a362e7ea79436291106d494eb6
Isela Alarcón <isela.v.alarcon@gmail.com>
2022-12-13 15:38:49 +0100

    using the spread operator in data structures

--- script.js
+++ script.js
@@ -35,6 +35,9 @@ const restaurant = {
       `Order recived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is you delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
 };
 
 // ...AND ADDING THIS OPTIONS
@@ -45,72 +48,120 @@ restaurant.orderDelivery({
   starterIndex: 2,
 });
 
-// ASIGNATING A NEW VALUE IN AN ARRAY
-const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+// // ASIGNATING A NEW VALUE IN AN ARRAY
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
 
-const [x, y, z] = arr;
-console.log(x, y, z);
-console.log(arr);
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(arr);
 
-// EXPECTED OUTPUT:  Italian, vegetarian
-let [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
+// // EXPECTED OUTPUT:  Italian, vegetarian
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
 
-// EXPECTED OUTPUT:  vegetarian, italian
-const temp = main;
-main = secondary;
-secondary = temp;
-console.log(main, secondary);
+// // EXPECTED OUTPUT:  vegetarian, italian
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
 
-// TRICK TO HAVE THE SAME RESULT, EXPECTED OUTPUT:  vegetarian, italian
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
+// // TRICK TO HAVE THE SAME RESULT, EXPECTED OUTPUT:  vegetarian, italian
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
 
-//EXPECTED OUTPUT ["GARLIC BREAD", "PIZZA"]
-console.log(restaurant.order(2, 0));
+// //EXPECTED OUTPUT ["GARLIC BREAD", "PIZZA"]
+// console.log(restaurant.order(2, 0));
 
-// RECEIVE 2 RETURN VAÑUES FROM A FUNCTION,  EXPECTED OUTPUT "GARLIC BREAD pÌZZA"
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
+// // RECEIVE 2 RETURN VAÑUES FROM A FUNCTION,  EXPECTED OUTPUT "GARLIC BREAD pÌZZA"
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
 
-//NESTED DESCTRUCTURING
-const nested = [2, 4, [5, 6]];
-// const [i,,j] = nested
-const [i, , [j, k]] = nested;
-console.log(i, j, k);
+// //NESTED DESCTRUCTURING
+// const nested = [2, 4, [5, 6]];
+// // const [i,,j] = nested
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
 
-//DEFAULT VALUES
-const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r);
+// //DEFAULT VALUES
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r);
 
-// ///////////////////////////////////////////////DESTRUCTURING OBJECTS
-const { name, openingHours, categories } = restaurant;
-console.log('output', name, openingHours, categories);
+// // ///////////////////////////////////////////////DESTRUCTURING OBJECTS
+// const { name, openingHours, categories } = restaurant;
+// console.log('output', name, openingHours, categories);
 
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
 
-console.log('output test', name, openingHours, categories);
+// console.log('output test', name, openingHours, categories);
 
-const { menu = [], starterMenu: starters = [] } = restaurant;
-console.log('hey!', menu, starters);
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log('hey!', menu, starters);
 
-// mutating variables
-// let a = 111;
-// let b = 999;
+// // mutating variables
+// // let a = 111;
+// // let b = 999;
 
-// const obj = { a: 23, b: 7, c: 14 };
-// ({ a, b } = obj);
-// console.log(a, b);
+// // const obj = { a: 23, b: 7, c: 14 };
+// // ({ a, b } = obj);
+// // console.log(a, b);
 
-//NESTED OBJECTS
-const {
-  fri: { open, close },
-} = openingHours;
-console.log(open, close);
+// //NESTED OBJECTS
+// const {
+//   fri: { open, close },
+// } = openingHours;
+// console.log(open, close);
+
+// ///////////////////////////////////////////////SPREAD OPERATOR
+
+const array = [7, 8, 9];
+const badNewArr = [1, 2, array[0], array[1], array[2]];
+const newArr = [1, 2, ...array];
+console.log(newArr);
+console.log(...newArr);
+
+// ADDING A NEW ELEMENT IN THE ARRAY WITH SPREAD OPERATOR
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// COPY ARRAY
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//JOIN 2 ARRAYS
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+
+// ITERABLES : ARRAYS, STRINGS, MAPS, SETS, NOT OBJECTS
+
+const str = 'Isela';
+const letters = [...str, , ' A.'];
+console.log(letters);
+
+//Real world example
+const ingredients = [
+  prompt("let's make pasta! Ingredient 1"),
+  prompt("let's make pasta! Ingredient 2"),
+  prompt("let's make pasta! Ingredient 3"),
+];
+
+console.log(ingredients);
+
+// TO CALL THE PASTA FUNCTION
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// WITH SPREAD OPERATOR
+restaurant.orderPasta(...ingredients);
+
+//Objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Isela' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristourante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
