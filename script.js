cfa44a86c8391ff373b65af9e9ce900a90e70b85
Isela Alarcón <isela.v.alarcon@gmail.com>
2023-01-17 20:07:29 +0100

    coding challenge 4 part 2

--- script.js
+++ script.js
@@ -948,5 +948,17 @@ document.body.append(document.createElement('button'));
 document.querySelector('button').addEventListener('click', function () {
   //pasar el valor del text area a la consola al dar click
   const text = document.querySelector('textarea').value;
-  console.log(text);
+  const rows = text.split('\n');
+  console.log(rows);
+
+  // to loop inside an array
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)} ${'✅'.repeat(i + 1)}`);
+  }
 });
